c30bcf103957e6b039896ce35b93b448b28c3f18
Luc-coder <lucasmarquesrussi8@gmail.com>
2022-05-10 13:47:38 -0300

    algumas alteracoes

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Projeto_Mercado
 Projeto para aplicações
+
+colocar opacidade no mobile quando a aba de menu estiver em block;
+
+fazer a aba no menu dezaparecer depois de um clique;
+
+colocar footer no mobile;
+
+colocar uma coluna em 280px;
---
+++ js/index.js
@@ -0,0 +1,10 @@
+const botaoMenu = document.querySelector('.button')
+const menu = document.querySelector('.menu-lateral')
+
+
+botaoMenu.addEventListener('click', () =>{
+    menu.classList.toggle('menu-lateral-ativo')
+})
+close.addEventListener('click', () =>{
+    menu.classList.toggle('menu-lateral-ativo')
+})
